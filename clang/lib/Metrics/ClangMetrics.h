7f36bc347a15f24aa70a33c1245a21130161fb5d
Balint Cserni <cserni.balint.gabor@stud.u-szeged.hu>
2018-04-20 14:55:57 +0200

    Fix bug in clangMetrics.

--- clang/lib/Metrics/ClangMetrics.h
+++ clang/lib/Metrics/ClangMetrics.h
@@ -208,25 +208,9 @@ private:
 };
 
 
-class ClangMetrics {
+class ClangMetrics
+{
 private:
-  struct Comparator
-  {
-    bool operator()(std::tuple<clang::FileID, unsigned, unsigned> a, std::tuple<clang::FileID, unsigned, unsigned> b) const
-    {
-      if (std::get<0>(a) < std::get<0>(b))
-        return true;
-
-      if (std::get<1>(a) < std::get<1>(b))
-        return true;
-
-      if (std::get<1>(a) > std::get<1>(b))
-        return false;
-
-      return std::get<2>(a) < std::get<2>(b);
-    }
-  };
-
   struct FileIDHasher
   {
     size_t operator()(const FileID& id) const
@@ -327,52 +311,8 @@ private:
   // Contains function metrics calculated per TU.
   std::unordered_map<const clang::DeclContext*, FunctionMetricsData> myFunctionMetrics;
 
-  /*// Contains all the classes in the program.
-  std::unordered_set<const clang::Decl*> myClasses;
-
-  // Contains all the methods and functions in the program.
-  std::unordered_set<const clang::DeclContext*> myFunctions;
-
-  // Contains all the enums (including C++ 11 strongly typed enums) in the program.
-  std::unordered_set<const clang::EnumDecl*> myEnums;
-
-  // Contains all the namespaces in the program.
-  std::unordered_set<const clang::NamespaceDecl*> myNamespaces;
-
-  // Maps classes/structs/unions defined within functions to the functions.
-  // If a function has no "inside" classes/structs/unions, it won't be found in this map.
-  std::unordered_map<const clang::DeclContext*, std::unordered_set<const clang::Decl*>> myInsideClassesByFunctions;
-
-  // Maps McCC to functions. If a function has an McCC of 1, it won't be found in this map.
-  // Note that the values stored here are one less than the final McCC.
-  //std::unordered_map<const clang::DeclContext*, unsigned> myMcCCByFunctions;
-
-  // Halstead operators (first) and operands (second) by functions.
-  //std::unordered_map<const clang::DeclContext*, HalsteadStorage> myHalsteadByFunctions;
-
-  // Maps inner classes to their "outside" class.
-  // If the class has no inner classes, it won't be found in this map.
-  std::unordered_map<const clang::Decl*, std::unordered_set<const clang::Decl*>> myInnerClassesByClasses;
-
-  // Maps ObjC Interfaces by Implementation.
-  std::unordered_map<const clang::ObjCContainerDecl*, const clang::ObjCContainerDecl*> myObjCInterfaceByImplementations;
-
-  // Maps methods to their class.
-  // If the class has no methods, it won't be found in this map.
-  std::unordered_map<const clang::Decl*, std::unordered_set<const clang::Decl*>> myMethodsByClasses;
-
-  // Maps namespaces inside another namespace to this "outside" namespace.
-  // If the namespace has no inner namespaces, it won't be found in this map.
-  std::unordered_map<const clang::NamespaceDecl*, std::unordered_set<const clang::NamespaceDecl*>> myInnerNamespacesByNamespaces;
-
-  // Maps classes/structs/unions to their namespace. If a class/struct/union is in the global namespace, it won't be found in this map.
-  std::unordered_map<const clang::NamespaceDecl*, std::unordered_set<const clang::Decl*>> myClassesByNamespaces;
-
-  // Maps enums (including C++ 11 strongly typed enums) to their namespace. If an enum is in the global namespace, it won't be found in this map.
-  std::unordered_map<const clang::NamespaceDecl*, std::unordered_set<const clang::EnumDecl*>> myEnumsByNamespaces;*/
-
   // Stores locations where there are semicolons. A single record is a pair of row/column within the file.
-  std::set<std::tuple<clang::FileID, unsigned, unsigned>, Comparator> mySemicolonLocations;
+  std::set<std::tuple<clang::FileID, unsigned, unsigned>> mySemicolonLocations;
 
   // McCC per file. If a file has an McCC of 1, it won't be found in this map.
   // Note that the values stored here are one less than the final McCC.
