26c4ae9b7837ac431f138ee088090bbf3786dc93
Siket Peter <pepe@inf.u-szeged.hu>
2020-01-08 11:03:59 +0100

    Add missing FS initialization

--- clang-tools-extra/clang-tidy/ClangTidyOptions.cpp
+++ clang-tools-extra/clang-tidy/ClangTidyOptions.cpp
@@ -217,7 +217,10 @@ FileOptionsProvider::FileOptionsProvider(
     const ClangTidyOptions &OverrideOptions,
     const FileOptionsProvider::ConfigFileHandlers &ConfigHandlers)
     : DefaultOptionsProvider(GlobalOptions, DefaultOptions),
-      OverrideOptions(OverrideOptions), ConfigHandlers(ConfigHandlers) {}
+      OverrideOptions(OverrideOptions), ConfigHandlers(ConfigHandlers) {
+  if (!FS)
+    FS = llvm::vfs::getRealFileSystem();
+}
 
 // FIXME: This method has some common logic with clang::format::getStyle().
 // Consider pulling out common bits to a findParentFileWithName function or
